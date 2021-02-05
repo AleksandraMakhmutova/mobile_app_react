2269a2762fe7cfc8c4244581fbfe9b92fce50967
HoneyMS <Honeyms@mail.ru>
2021-02-05 14:13:42 +0300

    add responsive iconfolder

--- client/src/Components/MainPage/FotoPart/index.js
+++ client/src/Components/MainPage/FotoPart/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import style from './style.module.css'
-
+import cn from 'classnames'
 function FotoPart() {
 	return (
 		<div className={style.container}>
 Hello
+
+<div className={style.folder}>
+<i className={`fas fa-folder-plus fa-2x ${cn(style.icon)}`}></i></div>
+
 		</div>
 	)
 }
